4505b7889899b47897b53edfeb3770679eaf0707
emilylim1406 <emilylim1406@gmail.com>
2026-07-17 01:52:48 +0800

    newer ver of player controller

--- FPS Game FinalProject/Assets/Scripts/PlayerController.cs
+++ FPS Game FinalProject/Assets/Scripts/PlayerController.cs
@@ -3,62 +3,77 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
-    public float moveSpeed, gravityModifier, jumpingPower, runSpeed;
+
+    public float moveSpeed = 8f;
+    public float gravityModifier = 4f;
+    public float jumpingPower = 9f;
+    public float runSpeed = 16f;
+
     public CharacterController charCon;
     private Vector3 moveInput;
+
     public Transform camTrans;
     public Animator anim;
+
     int jumpAgain;
 
     public GameObject bullet;
     public Transform firePoint;
 
-    public float mouseSensitivity;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float mouseSensitivity = 3f;
+
     void Start()
     {
+        if (charCon == null)
+            charCon = GetComponent<CharacterController>();
 
+        Debug.Log("PlayerController Started");
+
+        if (charCon == null)
+            Debug.LogError("CharacterController is NULL!");
+
+        if (camTrans == null)
+            Debug.LogError("Camera Transform is NULL!");
+
+        if (anim == null)
+            Debug.LogError("Animator is NULL!");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log($"Horizontal = {h}  Vertical = {v}");
 
         float yStore = moveInput.y;
 
-        //move based on player facing direction
-        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");//forward is Z or blue axis
-        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");//right is X or red axis
+        Vector3 vertMove = transform.forward * v;
+        Vector3 horiMove = transform.right * h;
 
         moveInput = vertMove + horiMove;
-        moveInput.Normalize();//to normalize the speed of diagonal movement, such as A&W
+        moveInput.Normalize();
 
         if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveInput = moveInput * runSpeed;
-        }
+            moveInput *= runSpeed;
         else
-        {
-            moveInput = moveInput * moveSpeed;
-        }
+            moveInput *= moveSpeed;
 
         moveInput.y = yStore;
 
-        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
-
-        if (charCon.isGrounded)//detect the ground
+        if (charCon.isGrounded)
         {
             moveInput.y = -1f;
-            moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 moveInput.y = jumpingPower;
                 jumpAgain = 2;
             }
-
+        }
+        else
+        {
+            moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
         }
 
         if (jumpAgain > 0 && Input.GetKeyDown(KeyCode.Space))
@@ -67,33 +82,34 @@ public class PlayerController : MonoBehaviour
             jumpAgain--;
         }
 
-
-
         charCon.Move(moveInput * Time.deltaTime);
 
-        Debug.Log(moveInput.magnitude);
         float horizontalSpeed = new Vector3(charCon.velocity.x, 0, charCon.velocity.z).magnitude;
-        anim.SetFloat("moveSpeed", horizontalSpeed);
 
+        if (anim != null)
+            anim.SetFloat("moveSpeed", horizontalSpeed);
 
-        //Player looking rotation (right and left)
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-        Debug.Log(horizontalSpeed);
+        transform.Rotate(Vector3.up * mouseX * mouseSensitivity);
 
-        if (Input.GetMouseButtonDown(0))
+        if (camTrans != null)
+        {
+            camTrans.Rotate(Vector3.left * mouseY * mouseSensitivity);
+        }
+
+        if (bullet != null && firePoint != null && Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
+
             if (Physics.Raycast(camTrans.position, camTrans.forward, out hit))
             {
                 firePoint.LookAt(hit.point);
             }
             else
             {
-                firePoint.LookAt(camTrans.position + (camTrans.forward * 30f));
-                //looking at the centre of the screen
+                firePoint.LookAt(camTrans.position + camTrans.forward * 30f);
             }
 
             Instantiate(bullet, firePoint.position, firePoint.rotation);
