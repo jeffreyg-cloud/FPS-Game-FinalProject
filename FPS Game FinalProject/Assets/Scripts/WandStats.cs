7ce1a070747af6fbf18221aca045a77bfe53db63
emilylim1406 <emilylim1406@gmail.com>
2026-07-22 23:48:27 +0800

    wand tht can swtch with attacks

---
+++ FPS Game FinalProject/Assets/Scripts/WandStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class WandStats : MonoBehaviour
+{
+    public float damage = 10f;
+    public float manaCost = 5f;
+    public Transform firePoint;
+    public GameObject effectPrefab;
+    public Animator wandAnimator;
+}
